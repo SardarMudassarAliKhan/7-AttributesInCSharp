c66ad042c2bce52d7826af8d2d052a45ef44b8ae
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-30 20:42:38 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Custom attribute definition
+public class MyAttribute : Attribute
+{
+    public string Description { get; }
+
+    public MyAttribute(string description)
+    {
+        Description = description;
+    }
+}
+
+// Applying the custom attribute to a class
+[My("This is a sample class")]
+class MyClass
+{
+    // Applying the custom attribute to a method
+    [My("This is a sample method")]
+    public void MyMethod()
+    {
+        Console.WriteLine("Executing MyMethod");
+    }
+}
+
+class Program
+{
+    [My("This is a Main method")]
+    static void Main(string[] args)
+    {
+        // Retrieving and using attributes at runtime
+        Type type = typeof(MyClass);
+        var classAttribute = (MyAttribute)Attribute.GetCustomAttribute(type, typeof(MyAttribute));
+        Console.WriteLine($"Class Attribute: {classAttribute.Description}");
+
+        var method = typeof(MyClass).GetMethod("MyMethod");
+        var methodAttribute = (MyAttribute)Attribute.GetCustomAttribute(method, typeof(MyAttribute));
+        Console.WriteLine($"Method Attribute: {methodAttribute.Description}");
+
+        MyClass myObj = new MyClass();
+        myObj.MyMethod();
+    }
+}
